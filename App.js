187d9c7da4ceb0ca290cce1c05872fe6dd008f96
Juan Gutierres <juan_g94@outlook.com>
2021-09-10 00:44:45 -0500

    cambio4

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, ScrollView, TextInput, Pressable, Image, TouchableOpacity, } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, ScrollView, TextInput, Pressable, Image, TouchableOpacity, Keyboard, } from 'react-native';
 import { Picker } from "@react-native-picker/picker";
 
 class App extends React.Component {
@@ -23,9 +23,18 @@ class App extends React.Component {
       subT: 0,
       iva: 0,
       total: 0,
-
-      
+      diferido: 0,
+            
     }
+      this.contador_1 = this.contador_1.bind(this);
+      this.contador_2 = this.contador_2.bind(this);
+      this.contador_3 = this.contador_3.bind(this);
+      this.contador_4 = this.contador_4.bind(this);
+
+    if (this.state.tipo == "Debito") {
+      this.setState({ diferido: 0 })
+    } 
+
   }
   setData = () => {
     try {
@@ -58,9 +67,48 @@ class App extends React.Component {
     }
   }
 
+ contador_1() {
+   const { value1: ct } = this.state;
+   if (this.state.value1 > 0 ) {
+    this.setState({
+      value1: ct - 1
+    });
+   }
+ 
+  
+ }
 
+ 
+
+ contador_2() {
+  const { value1: ct } = this.state;
+  this.setState({ value1: ct + 1 });
+
+}
+
+
+contador_3() {
+  const { value2: ct3 } = this.state;
+  if (this.state.value2 > 0 ) {
+   this.setState({
+     value2: ct3 - 1
+   });
+  }
+}
+
+  contador_4() {
+    const { value2: ct4 } = this.state;
+    
+     this.setState({
+       value2: ct4 + 1
+     });
+    
+  }
 
   render(){
+
+    const { value1, value2 } = this.state;
+
     return (
       
 
@@ -79,14 +127,19 @@ class App extends React.Component {
 
 
 
-      
-    <Text style={styles.title}>Canasta $19.99</Text>
+      <Text style={styles.textx} >Canasta $20.00</Text>
+
+    <Image source={require('./assets/canasta.jpg')} 
+   style={{ width: 100, height: 100, alignContent: 'center', marginLeft: "33%" , marginBottom: 0 }} />
+    
         
     <View style={ styles.cont1 }>
 
     <View style={ styles.cont2 }>
 
       <TouchableOpacity 
+       
+        onPress={ this.contador_1 }
         style={ styles.cont3 }>
 
         <Text style={styles.cont4} >-</Text>
@@ -94,17 +147,57 @@ class App extends React.Component {
       </TouchableOpacity>  
 
       <View style={styles.cont5}>
-      <Text style={styles.contador} >0</Text>
+      <Text style={styles.contador} >{ value1 }</Text>
       </View>
 
-      <TouchableOpacity style={ styles.cont3 }>
+      <TouchableOpacity style={ styles.cont3 }
+         onPress={ this.contador_2 }
+      >
+
         <Text style={styles.cont4} >+</Text>
       </TouchableOpacity>
 
     </View>
+    <Text style={styles.title3}>Total = $ { value1 * 20 } </Text>
 
-     
     </View>
+
+
+    <Text style={styles.textx}>Frutas $15.00</Text>
+
+    <Image source={require('./assets/frutas.jpg')} 
+   style={{ width: 130, height: 100, alignContent: 'center', marginLeft: "30%" , marginBottom: 0 }} />
+
+    
+        
+        <View style={ styles.cont1 }>
+    
+        <View style={ styles.cont2 }>
+    
+          <TouchableOpacity 
+           
+            onPress={ this.contador_3 }
+            style={ styles.cont3 }>
+    
+            <Text style={styles.cont4} >-</Text>
+    
+          </TouchableOpacity>  
+    
+          <View style={styles.cont5}>
+          <Text style={styles.contador} >{ value2 }</Text>
+          </View>
+    
+          <TouchableOpacity style={ styles.cont3 }
+             onPress={ this.contador_4 }
+          >
+    
+            <Text style={styles.cont4} >+</Text>
+          </TouchableOpacity>
+    
+        </View>
+        <Text style={styles.title3}>Total = $ { value2 * 15 } </Text>
+    
+        </View>
 
 
         {/* <Pressable style={styles.button2} onPress={this.setData.bind(this)}>
@@ -126,12 +219,14 @@ class App extends React.Component {
         />
         <Text style={styles.title}>Identification Card</Text>
         <TextInput
+        keyboardType="numeric"
           style={styles.input}
           value = { this.state.cedula }
           onChangeText={ val => this.setState({ cedula: val })}
         />
         <Text style={styles.title}>Card Details</Text>
         <TextInput
+        keyboardType="numeric"
           style={styles.input}
           value = { this.state.tarjeta }
           onChangeText={ val => this.setState({ tarjeta: val })}
@@ -144,6 +239,7 @@ class App extends React.Component {
         />
          <Text style={styles.title}>CVV</Text>
         <TextInput
+            keyboardType="numeric"
           style={styles.input}
           value = { this.state.cvv }
           onChangeText={ val => this.setState({ cvv: val })}
@@ -162,23 +258,36 @@ class App extends React.Component {
 
         
          <Text style={styles.title}>Credit/Debit</Text>
-        <TextInput
-          style={styles.input}
-          value = { this.state.tipo }
-          onChangeText={ val => this.setState({ tipo: val })}
-        />
+         <Picker style={styles.input} onValueChange={(itemValue, itemIndex) => this.setState({ tipo: itemValue, diferido: 0 }) }>
+            <Picker.Item label="Debito" value= { "Debito" } />
+            <Picker.Item label="Credito" value= { "Credito"} />
+          
+       
+            </Picker>
+              { this.state.tipo == "Credito" ? 
+              <>
+              <Picker style={styles.input} onValueChange={(itemValue, itemIndex) => this.setState({diferido: itemValue})}>
+              <Picker.Item label="Sin Diferir" value= { 0 } />
+              <Picker.Item label="3 Monts" value= { 3 } />
+              <Picker.Item label="6 Monts" value= { 6 } />
+              <Picker.Item label="9 Monts" value= { 9 } />
+              </Picker>
+             
+              </>
+           : 
+           null
+          }
+       
 
-        
-
-        <Text style={styles.title2}>SubTotal =   $78.5</Text>    
-        <Text style={styles.title2}>iva =   $9.42</Text> 
-        <Text style={styles.title2}>Total = $87.92</Text>   
+        <Text style={styles.title2}>SubTotal =   $ {  ( (value1 * 20 ) + ( value2 * 15 ) - (((value1 * 20 ) + ( value2 * 15 )) /1.12) ).toFixed(2) }</Text>    
+        <Text style={styles.title2}>iva =   $ { (((value1 * 20 ) + ( value2 * 15 )) /1.12).toFixed(2)  }</Text> 
+        <Text style={styles.title2}>Total = $ { ((value1 * 20 ) + ( value2 * 15 )).toFixed(2)  }</Text>   
 
         {/* <button onClick={this.setData.bind(this)}>Pay</button> */}
 
 
         <Pressable style={styles.button} onPress={this.setData.bind(this)}>
-           <Text style={styles.text}>Pay $87.92</Text>
+           <Text style={styles.text}>Pay $ { (value1 * 20 ) + ( value2 * 15 ) }</Text>
          </Pressable>
   
     </View>
@@ -196,7 +305,7 @@ class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fcfcfc',
+    backgroundColor: '#fff',
     paddingVertical: 20,
     paddingHorizontal: 30,
   },
@@ -204,7 +313,7 @@ const styles = StyleSheet.create({
     flex: 1,
     width: 200,
     height: 100,
-    backgroundColor: '#fcfcfc',
+    backgroundColor: '#fff',
     paddingVertical: 20,
     paddingHorizontal: 30,
   },
@@ -226,7 +335,15 @@ const styles = StyleSheet.create({
     color: '#252626',
     fontSize: 18,
     fontWeight: 'bold',
-    marginVertical: 10,
+    marginVertical: 5,
+  },
+  title3: {
+    color: '#252626',
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginVertical: 5,
+    marginBottom:10,
+    marginTop: 5
   },
   input: {
     borderWidth: 1,
@@ -287,6 +404,15 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     letterSpacing: 0.25,
     color: 'white',
+  },
+  textx: {
+    textAlign: 'center',
+    fontSize: 20,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: '#252626',
+    marginBottom: 5,
   },
 
   cont1: {
